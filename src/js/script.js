455dffdb73ce9eedef75675a2f33ed801dddddfc
kaio <kaitozhiroki@gmail.com>
2026-04-04 19:57:37 -0300

    feat: Adicionando variáveis para informações.

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+var nome = prompt("Digite o nome do vinho: ");
+var tipo = prompt("Digite o tipo do vinho: ");
+var safra = parseInt(prompt("Digite a safra do vinho (idade): "));
+var quantidade = parseInt(prompt("Digite a quantidade de vinhos: "));
