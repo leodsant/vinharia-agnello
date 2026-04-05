d7e5c7e38f3039a5ab8d550b8a646a85a8539cfe
Leonardo <contato.leodsantos@gmail.com>
2026-04-05 13:26:33 -0300

    feat: melhorando uma linha do arquivo javascript

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,7 @@ var tipo = prompt("Digite o tipo do vinho: ");
 var safra = parseInt(prompt("Digite a safra do vinho (idade): "));
 var quantidade = parseInt(prompt("Digite a quantidade de vinhos: "));
 
-alert("Cadastro realizado! Veja os detalhes no console.");
-alert("A seguir, veja os detalhes do vinho no console.");
+alert("Cadastro realizado! Veja os detalhes do vinho no console.");
 
 console.log(`Nome do vinho: ${nome}`);
 console.log(`Tipo do vinho: ${tipo}`);
